049395e354e6e16d0c1875dc3d683e0789d3f9ed
Gabriel Carvalho <gcarvalhomartins23@gmail.com>
2022-12-03 19:39:31 -0400

    Primeiro commit

---
+++ UrnaEletronica/UrnaEletronica/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UrnaEletronica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
