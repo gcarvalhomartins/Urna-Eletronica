4b80dbfc2616acc5a9fb817f379ba943af8e769f
Gabriel Carvalho <gcarvalhomartins23@gmail.com>
2022-12-03 23:16:20 -0400

    adicionando DAO

---
+++ UrnaEletronica/UrnaEletronica.Domain/DAO/CandidatoDAO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UrnaEletronica.Domain.Models;
+
+namespace UrnaEletronica.Domain.DAO
+{
+    internal class CandidatosDao
+    {
+        public static List<Candidato> ListaDeCandidatos =new List<Candidato>();
+    }
+}
---
+++ UrnaEletronica/UrnaEletronica.Domain/Models/Candidato.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace UrnaEletronica.Domain.Models
+{
+	public class Candidato
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public int Numero { get; set; }
+		public string CargoDisputado { get; set; }
+		public string Partido { get; set; }
+
+	}
+}
