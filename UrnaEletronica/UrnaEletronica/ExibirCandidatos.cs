2cbc3129eb853f15749c56daf8ff8cc0f0c17b84
Gabriel Carvalho <gcarvalhomartins23@gmail.com>
2022-12-11 09:40:24 -0400

    gerenciando melhor os serviços de candidatos fixos

--- UrnaEletronica/UrnaEletronica/ExibirCandidatos.cs
+++ UrnaEletronica/UrnaEletronica/ExibirCandidatos.cs
@@ -9,17 +9,21 @@ namespace UrnaEletronica.CLI
 
 	internal class ExibirCandidatos
 	{
+        
         static CandidatoExtraServices _candidadoExtrasServices = new CandidatoExtraServices();
+
+        static CandidatosFixosServices _candidatosFixosServices = new CandidatosFixosServices();
 		public static void MenuExibirCandidato()
-		{
+		{   
             string respostaUsuario;
             bool pediuPraSair = false;
             do
             {
+             
 
                 Console.WriteLine("Você está em Vizualiação de Candidato =) ");
                 Console.WriteLine("A - Para Vizualizar os Candidatos ");
-                Console.WriteLine("B - Para adicionar Candidatos ");
+                Console.WriteLine("B - Para adicionar Candidatos Fixos ");
 
                 respostaUsuario = Console.ReadLine();
 
@@ -29,7 +33,7 @@ namespace UrnaEletronica.CLI
                         Exibir();
                         break;
                     case "b":
-                        CandidatosFixos();
+                        CandidatosFixosServices.AdicionarCandidatosFixos();
                         break;
                     case "c":
                         Console.WriteLine();
@@ -43,7 +47,9 @@ namespace UrnaEletronica.CLI
         }
         public static void Exibir()
         {
-            foreach( var candidato in CandidatosDao.ListaDeCandidatos)
+            
+
+            foreach ( var candidato in CandidatosDao.ListaDeCandidatos)
             {   
                 Console.WriteLine("Id do Candidato : " + candidato.Id);
                 Console.WriteLine("Nome do Candidato : " + candidato.Nome);
@@ -52,42 +58,6 @@ namespace UrnaEletronica.CLI
                 Console.WriteLine("Cargo Disputado :" + candidato.CargoDisputado);
             }
         }
-        public static void CandidatosFixos()
-        {
-            
-            Candidato candidatos1 = new Candidato();
-            candidatos1.Id = 2323;
-            candidatos1.Nome = "José";
-            candidatos1.Numero = 13;
-            candidatos1.Partido = "PVV";
-            candidatos1.CargoDisputado = "Presidencia";
-
-            Candidato candidatos2 = new Candidato();
-            candidatos2.Id = 4675;
-            candidatos2.Nome = "Paulo";
-            candidatos2.Numero = 13;
-            candidatos2.Partido = "KDI";
-            candidatos2.CargoDisputado = "Presidencia";
-
-            Candidato candidatos3 = new Candidato();
-            candidatos3.Id = 9090;
-            candidatos3.Nome = "Marquinhos";
-            candidatos3.Numero = 13;
-            candidatos3.Partido = "KEJ";
-            candidatos3.CargoDisputado = "Governador";
-
-            Candidato candidatos4 = new Candidato();
-            candidatos4.Id = 5648;
-            candidatos4.Nome = "Yoham";
-            candidatos4.Numero = 13;
-            candidatos4.Partido = "SWE";
-            candidatos4.CargoDisputado = "Governador";
-
-            _candidadoExtrasServices.AdicionarVisitanteNaLista(candidatos1);
-            _candidadoExtrasServices.AdicionarVisitanteNaLista(candidatos2);
-            _candidadoExtrasServices.AdicionarVisitanteNaLista(candidatos3);
-            _candidadoExtrasServices.AdicionarVisitanteNaLista(candidatos4);
-
-        }
+       
 	}
 }
